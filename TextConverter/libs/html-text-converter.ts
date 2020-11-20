1a666a9f8aeaae517b3f6e9c9f635c04d12d3774
Alessio Coser <alessio.coser@xpeppers.com>
2020-11-20 08:38:36 +0100

    update

--- TextConverter/libs/html-text-converter.ts
+++ TextConverter/libs/html-text-converter.ts
@@ -1,5 +1,4 @@
 import { readFileSync } from 'fs'
-import StringUtils from './string-utils'
 
 class HtmlTextConverter {
   private _filename: string
@@ -11,7 +10,41 @@ class HtmlTextConverter {
   convertToHtml(): string {
     const text = readFileSync(this._filename, 'utf8')
 
-    return StringUtils.basicHtmlEncode(text)
+    return this._basicHtmlEncode(text)
+  }
+
+  private _basicHtmlEncode(source: string): string {
+    let i: number = 0
+    const result: string[] = []
+    let convertedLine: string[] = []
+    let characterToConvert: string = source.charAt(i)
+    while (i <= source.length) {
+      i++
+      switch (characterToConvert) {
+        case '<':
+          convertedLine.push('&lt')
+          break
+        case '>':
+          convertedLine.push('&gt')
+          break
+        case '&':
+          convertedLine.push('&amp')
+          break
+        case '\n':
+          result.push(convertedLine.join(''))
+          convertedLine = []
+          break
+        default:
+          convertedLine.push(characterToConvert)
+      }
+
+      characterToConvert = source.charAt(i)
+    }
+
+    result.push(convertedLine.join(''))
+    convertedLine = []
+
+    return result.join('<br />')
   }
 }
 
